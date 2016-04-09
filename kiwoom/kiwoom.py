002bba612db15372be5ee681d73171950eabe10a
SShim from PC <mechurak@gmail.com>
2016-04-10 02:16:40 +0900

    Restructured

---
+++ kiwoom/kiwoom.py
@@ -0,0 +1,165 @@
+from PyQt4.QAxContainer import *
+from PyQt4.QtCore import *
+
+
+class Kiwoom:
+    data = {"조건식_list": [[0, "조건식0", "매도신호", False]],  # [인덱스, 조건명, 신호종류, 적용유무]
+            "계좌번호": "12345",
+            "잔고_dic": {"00000": {"종목명": "테스트0", "현재가": 2000, "매입가": 1000, "수익율": 0.5, "매수전략": True, "매도전략": True},
+                       "00001": {"종목명": "테스트1", "현재가": 2000, "매입가": 1000, "수익율": 0.5, "매수전략": False, "매도전략": False}
+                       },
+            }
+
+    def __init__(self, the_callback):
+        self.callback = the_callback
+
+        self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.ocx.connect(self.ocx, SIGNAL("OnReceiveTrData(QString, QString, QString, QString, QString, int, QString, QString, QString)"), self.OnReceiveTrData)
+        self.ocx.connect(self.ocx, SIGNAL("OnReceiveRealData(QString, QString, QString)"), self.OnReceiveRealData)
+        self.ocx.connect(self.ocx, SIGNAL("OnReceiveMsg(QString, QString, QString, QString)"), self.OnReceiveMsg)
+        self.ocx.connect(self.ocx, SIGNAL("OnReceiveChejanData(QString, int, QString)"), self.OnReceiveChejanData)
+        self.ocx.connect(self.ocx, SIGNAL("OnEventConnect(int)"), self.OnEventConnect)
+        self.ocx.connect(self.ocx, SIGNAL("OnReceiveCondition(QString, QString, QString, QString)"), self.OnReceiveCondition)
+        self.ocx.connect(self.ocx, SIGNAL("OnReceiveTrCondition(QString, QString, QString, int, int)"), self.OnReceiveTrCondition)
+        self.ocx.connect(self.ocx, SIGNAL("OnReceiveConditionVer(int, QString)"), self.OnReceiveConditionVer)
+
+        self.login()
+
+    def OnReceiveTrData(self, sScrNo, sRQName, sTrCode, sRecordName, sPreNext, nDataLength, sErrorCode, sMessage, sSplmMsg):
+        print("(OnReceiveTrData) ", sScrNo, sRQName, sTrCode, sRecordName, sPreNext, nDataLength, sErrorCode, sMessage, sSplmMsg)
+        if sRQName == "주식기본정보요청":
+            name = self.ocx.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sTrCode, "", sRQName, 0, "종목명")
+            volume = self.ocx.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sTrCode, "", sRQName, 0, "거래량")
+            retStr = ""
+            retStr += "종목명: " + name.strip() + "\n"
+            retStr += "거래량: " + volume.strip() + "\n"
+            self.callback.on_print(retStr)
+
+        elif sRQName == "계좌평가잔고내역요청":
+            print("계좌평가잔고내역요청")
+            count = self.ocx.dynamicCall("GetDataCount(QString)", ["계좌평가잔고개별합산"])
+            print("count: ", count)
+            잔고_dic = self.data["잔고_dic"]
+            잔고_dic.clear()
+            for i in range(0, count):
+                code = self.ocx.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sTrCode, "", sRQName, i, "종목번호")
+                name = self.ocx.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sTrCode, "", sRQName, i, "종목명")
+                cur_price = self.ocx.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sTrCode, "", sRQName, i, "현재가")
+                buy_price = self.ocx.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sTrCode, "", sRQName, i, "매입가")
+                earnings_rate = self.ocx.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sTrCode, "", sRQName, i, "수익률(%)")
+                code = code.strip()
+                name = name.strip()
+                cur_price = int(cur_price.strip())
+                buy_price = int(buy_price.strip())
+                earnings_rate = float(earnings_rate.strip())
+                earnings_rate /= 100
+                print("수익률", earnings_rate)
+                print(name, "현재가: ", cur_price, "매입가: ", buy_price, "수익률: ", earnings_rate)
+                잔고_dic[code] = {"종목명": name, "현재가": cur_price, "매입가": buy_price, "수익율": earnings_rate, "매수전략": True, "매도전략": True}
+            self.callback.on_data_updated(["잔고_dic"])
+
+    def OnReceiveRealData(self, sJongmokCode, sRealType, sRealData):
+        print("(OnReceiveRealData) ", sJongmokCode, ", ", sRealType, ", ", sRealData)
+        잔고_dic = self.data["잔고_dic"]
+        if sJongmokCode in 잔고_dic:
+            매수전략_list = 잔고_dic[sJongmokCode]["매수전략"]
+            for 매수전략 in 매수전략_list:
+                매수전략.onRealData(sJongmokCode, sRealType, sRealData)
+
+            매도전략_list = 잔고_dic[sJongmokCode]["매도전략"]
+            for 매도전략 in 매도전략_list:
+                매도전략.onRealData(sJongmokCode, sRealType, sRealData)
+
+    def OnReceiveMsg(self, sScrNo, sRQName, sTrCode, sMsg):
+        print("(OnReceiveMsg) ", sScrNo, sRQName, sTrCode, sMsg)
+
+    def OnReceiveChejanData(self, sGubun, nItemCnt, sFidList):
+        print("(OnReceiveChejanData) ", sGubun, nItemCnt, sFidList)
+
+    def OnEventConnect(self, nErrCode):
+        if nErrCode == 0:
+            print("로그인 성공")
+            self.callback.on_print("로그인 성공")
+            account_num = self.ocx.dynamicCall("GetLoginInfo(QString)", ["ACCNO"])
+            account_num = account_num[:-1]
+            account_list = account_num.split(";")
+            self.data["계좌번호"] = account_list[0]
+            self.callback.on_data_updated(["계좌번호"])
+            self.callback.on_connected()
+
+    def OnReceiveCondition(self, strCode, strType, strConditionName, strConditionIndex):
+        print("(OnReceiveCondition) ", strCode, strType, strConditionName, strConditionIndex)
+
+    def OnReceiveTrCondition(self, sScrNo, strCodeList, strConditionName, nIndex, nNext):
+        print("(OnReceiveTrCondition) ", sScrNo, strCodeList, strConditionName, nIndex, nNext)
+        code_list_str = strCodeList[:-1]  # 마지막 ";" 제거
+        code_list = code_list_str.split(';')
+        print(code_list)
+        for code in code_list:
+            name = self.ocx.dynamicCall("GetMasterCodeName(QString)", [code])
+            print(name)
+
+    def OnReceiveConditionVer(self, lRet, sMsg):
+        print("(OnReceiveConditionVer) ", lRet, sMsg)
+        condition_ret = self.ocx.dynamicCall("GetConditionNameList()")
+        condition_ret = condition_ret[:-1]  # 마지막 ";" 제거
+        print(condition_ret)
+        condition_list_raw = condition_ret.split(";")
+        print(condition_list_raw)
+        condition_list = self.data["조건식_list"]
+        condition_list.clear()
+        for condition_with_index in condition_list_raw:
+            if condition_with_index == "":
+                continue
+            cur = condition_with_index.split("^")
+            cur_list = [int(cur[0]), cur[1], "미지정", False]
+            condition_list.append(cur_list)
+        print(condition_list)
+        self.callback.on_data_updated(["조건식_list"])
+
+    ##############################################################
+    # Functions
+    ##############################################################
+
+    def login(self):
+        if self.ocx.dynamicCall("GetConnectState()") == 0:
+            self.ocx.dynamicCall("CommConnect()")
+
+    def refresh_condition_dic(self):
+        success = self.ocx.dynamicCall("GetConditionLoad()")
+        print(success)
+
+    def tr_condition_result(self, condition):
+        ret = self.ocx.dynamicCall("SendCondition(QString, QString, int, int)", "0101", condition[1], condition[0], 0)
+        print("SendCondition ret: ", ret)
+
+    def tr_balance(self):
+        account = self.data["계좌번호"]
+        print("계좌번호", account)
+        self.ocx.dynamicCall("SetInputValue(QString, QString)", "계좌번호", account)
+        self.ocx.dynamicCall("SetInputValue(QString, QString)", "조회구분", 2)
+        self.ocx.dynamicCall("CommRqData(QString, QString, int, QString)", "계좌평가잔고내역요청", "opw00018", 0, "0101")
+
+    def tr_code(self, 종목코드):
+        self.ocx.dynamicCall("SetInputValue(QString, QString)", "종목코드", 종목코드)
+        self.ocx.dynamicCall("CommRqData(QString, QString, int, QString)", "주식기본정보요청", "opt10001", 0, "0102")
+
+    def set_real_reg(self, 종목코드_list_str):
+        screen_num = "0103"
+        fid = "9001;10;13;21;41"
+        ret = self.ocx.dynamicCall("SetRealReg(QString, QString, QString, QString)", [screen_num, 종목코드_list_str, fid, "0"])
+        print("ret ", ret)
+
+
+class KiwoomCallback:
+    def on_connected(self):
+        pass
+
+    def on_data_updated(self, key_list):
+        pass
+
+    def on_print(self, log_str):
+        pass
+
+
+
--- window.py
+++ window.py
@@ -2,13 +2,14 @@ import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4 import uic
+from kiwoom.kiwoom import Kiwoom
+from kiwoom.kiwoom import KiwoomCallback
 
-class MyWindow(QMainWindow):
-    def __init__(self, kiwoom_util, my_data):
+
+class MyWindow(QMainWindow, KiwoomCallback):
+    def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi('test.ui', self)
-        self.kiwoom_util = kiwoom_util
-        self.my_data = my_data
+        self.ui = uic.loadUi('window.ui', self)
         self.ui.table_current.setHorizontalHeaderLabels(["test1", "test2", "test3"])
         self.ui.table_current.setItem(0, 0, QTableWidgetItem("test"))
         self.ui.table_current.setItem(0, 1, QTableWidgetItem("test0_1"))
@@ -17,56 +18,55 @@ class MyWindow(QMainWindow):
     @pyqtSlot(str)
     def on_account_changed(self, account):
         print("on_account_changed" + account)
-        self.my_data["계좌번호"] = account
+        kiwoom.data["계좌번호"] = account
 
     @pyqtSlot()
     def on_condition_refresh_btn_clicked(self):
         print("on_condition_refresh_clicked")
-        self.kiwoom_util.refresh_condition_dic()
+        kiwoom.refresh_condition_dic()
 
     @pyqtSlot()
     def on_balance_btn_clicked(self):
         print("on_balance_btn_clicked")
-        self.kiwoom_util.tr_balance()
+        kiwoom.tr_balance()
 
     @pyqtSlot()
     def on_print_my_data_btn_clicked(self):
         print("\n========== my data ==============")
-        for key, data in self.my_data.items():
+        for key, data in kiwoom.data.items():
             print(" (" + key + ") ", data)
 
     @pyqtSlot()
     def on_code_btn_clicked(self):
         print("on_code_btn_clicked")
         print("text(): " + self.ui.edit_code.text())
-        self.kiwoom_util.tr_code(self.ui.edit_code.text())
+        kiwoom.tr_code(self.ui.edit_code.text())
 
     @pyqtSlot()
     def on_register_real_btn_clicked(self):
         print("on_register_real_btn_clicked")
         print("text(): " + self.ui.edit_code.text())
-        self.kiwoom_util.set_real_reg(self.ui.edit_code.text())
+        kiwoom.set_real_reg(self.ui.edit_code.text())
 
     @pyqtSlot()
     def on_register_real_all_btn_clicked(self):
         print("on_register_real_all_btn_clicked")
-        잔고_dic = self.my_data["잔고_dic"]
+        잔고_dic = kiwoom.data["잔고_dic"]
         잔고코드_list = 잔고_dic.keys()
         잔고코드_list_str = ";".join(잔고코드_list)
         print("잔고코드_list_str", 잔고코드_list_str)
-        self.kiwoom_util.set_real_reg(잔고코드_list_str)
+        kiwoom.set_real_reg(잔고코드_list_str)
 
     def on_connected(self):
-        account_list = self.my_data["계좌번호"]
-        self.ui.combo_account.addItems(account_list)
+        self.statusBar().showMessage("Connected")
 
-    def on_my_data_updated(self, key_list):
+    def on_data_updated(self, key_list):
         if "계좌번호" in key_list:
-            account = self.my_data["계좌번호"]
+            account = kiwoom.data["계좌번호"]
             self.ui.edit_account.setText(account)
 
         if "조건식_list" in key_list:
-            condition_list = self.my_data["조건식_list"]
+            condition_list = kiwoom.data["조건식_list"]
             for i in range(0, len(condition_list)):
                 self.ui.table_condition.setItem(i, 0, QTableWidgetItem(str(condition_list[i][0])))
                 self.ui.table_condition.setItem(i, 1, QTableWidgetItem(condition_list[i][1]))
@@ -85,7 +85,7 @@ class MyWindow(QMainWindow):
                 self.ui.table_condition.setCellWidget(i, 4, button)
 
         if "잔고_dic" in key_list:
-            balance_dic = self.my_data["잔고_dic"]
+            balance_dic = kiwoom.data["잔고_dic"]
             headers = list(list(balance_dic.values())[0].keys())
             self.ui.table_current.setColumnCount(len(headers))
             self.ui.table_current.setHorizontalHeaderLabels(headers)
@@ -98,3 +98,9 @@ class MyWindow(QMainWindow):
     def on_print(self, log_str):
         self.ui.txt_output.append(log_str)
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    kiwoom = Kiwoom(window)
+    window.show()
+    app.exec_()
