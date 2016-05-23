57fffff914d15f1d0d593972bbb029d3eac8279e
blueblud7 <blueblud@gmail.com>
2016-05-24 00:23:49 +0900

    kelly betting implementation

--- kiwoom/strategy/kelly.py
+++ kiwoom/strategy/kelly.py
@@ -1,12 +1,10 @@
 class KellyBetting:
-    def __init__(self):
-        print("kelly betting class")
     # basic batting
     # b : 1 + ratio ex) 1.05
     # p : winning probability
     # q : losing probability
     # x : % of total money to bet per a game
-    def betting(self, totalamount, b, p, q):
+    def on_betting(self, totalamount, b, p, q):
         x = (b*p-q)/b
 
         return x*totalamount
