2414f3c2e063934d78c3276a9415c745a68c7178
blueblud7 <blueblud@gmail.com>
2016-05-25 00:00:34 +0900

    분할매수/매도
    분할매수/매도 시작

---
+++ kiwoom/strategy/accumulate.py
@@ -0,0 +1,10 @@
+"""
+분할 매수
+"""
+
+class accumulate:
+    def __init__(self):
+        print("분할매수")
+
+    def abuy(self, ratio, totalamount):
+        continue
---
+++ kiwoom/strategy/distribution.py
@@ -0,0 +1,10 @@
+"""
+분할 매도
+"""
+
+class distribution:
+    def __init__(self):
+        print("분할매도")
+
+    def asell(self, ratio, totalamount):
+        continue
