8474edf4d3fc6fabb28988aa43592a84463ffbee
blue <blueblud@gmail.com>
2016-07-25 19:14:40 +0900

    oil

--- kiwoom/temp/bs_test.py
+++ kiwoom/temp/bs_test.py
@@ -7,7 +7,8 @@ soup = BeautifulSoup(res, 'html.parser')
 
 #print(soup.prettify())
 
+# it shows the info such as oil, currency, and so on
 oils = soup.find_all('td', class_='8point')
 
 #for oil in oils:
-#print(oil)
+print(oils)
